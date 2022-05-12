b635f4dba601553235b1c05f2c85691bee63ba7b
Shawwal <Shawwal2.Shahid@live.uwe.ac.uk>
2022-05-12 13:18:06 +0100

    Quick fix

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62981e0e02f97f735961d649a3f281aebb4819442de6d81be4892e6833177ff
-size 4866
+oid sha256:e5343bdae14884b5c5298df9b05eaaabed2a1e947579e9f82558deff8657e9e0
+size 4905
