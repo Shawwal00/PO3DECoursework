2dd70c11704f2a0691b4c030691e77f545651fdc
Shawwal <Shawwal2.Shahid@live.uwe.ac.uk>
2022-05-12 02:25:51 +0100

    Rock camera

--- Assets/Scripts/Camera/FollowCamera.cs
+++ Assets/Scripts/Camera/FollowCamera.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04f3114ad64b96b11a36a96675956d26bbdfb3cbf09275909976041cff7f287d
-size 4534
+oid sha256:06229eb7aeade32a8acbef234364034d357ac9960aec48dfdea6fb2efc892ce4
+size 4727
--- Assets/Scripts/Ship/Catcher.cs
+++ Assets/Scripts/Ship/Catcher.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cd02e70de07f6019e5a7392b0a23fdc97bf5864eff4b56ab24110f8f5c3f08e
-size 1609
+oid sha256:e46829b4a23662c1e0106ca77ea6094d058c6a6e85454afc2c0e7e6bd6e28166
+size 1613
